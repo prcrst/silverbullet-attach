c6f52af58fe15d29113f1723e47a2339e6930d4c
prcrst <p-github@prcr.st>
2023-11-21 20:56:38 +0100

    Implement attaching a daily image

--- attach.ts
+++ attach.ts
@@ -1,6 +1,28 @@
 import { editor } from "$sb/silverbullet-syscall/mod.ts";
 
+// Stolen from silverbullet dates.ts
+function niceDate(d: Date): string {
+  function pad(n: number) {
+    let s = String(n);
+    if (s.length === 1) {
+      s = "0" + s;
+    }
+    return s;
+  }
+
+  return d.getFullYear() + "-" + pad(d.getMonth() + 1) + "-" + pad(d.getDate());
+}
+
 export async function attachImage() {
-  const uploadName = await editor.prompt("The name for the upload:")
-  editor.attachFile(uploadName!, "image/*");
+  const uploadName = await editor.prompt("The name for the upload:");
+  if (uploadName) {
+    editor.attachFile(uploadName, "image/*");
+  }
+}
+
+export async function attachDailyImage() {
+  const uploadName = await editor.prompt("The name for the daily image:", `images/daily/${niceDate(new Date())}.jpg`);
+  if (uploadName) {
+    editor.attachFile(uploadName!, "image/jpeg");
+  }
 }
