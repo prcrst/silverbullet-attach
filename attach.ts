47d91723951148fff49caa23ef0198a612c0f255
prcrst <p-github@prcr.st>
2023-11-22 10:01:50 +0100

    Cleanup + add slash commands

--- attach.ts
+++ attach.ts
@@ -1,28 +1,28 @@
 import { editor } from "$sb/silverbullet-syscall/mod.ts";
+import { niceDate, niceTime } from "$sb/lib/dates.ts";
 
-// Stolen from silverbullet dates.ts
-function niceDate(d: Date): string {
-  function pad(n: number) {
-    let s = String(n);
-    if (s.length === 1) {
-      s = "0" + s;
-    }
-    return s;
-  }
-
-  return d.getFullYear() + "-" + pad(d.getMonth() + 1) + "-" + pad(d.getDate());
-}
-
-export async function attachImage() {
-  const uploadName = await editor.prompt("The name for the upload:");
+export async function attachCameraImage() {
+  const uploadName = await editor.prompt("The name for the upload:", `images/camera/${niceDate(new Date())}_${niceTime(new Date())}.jpg`);
   if (uploadName) {
-    editor.attachFile(uploadName, "image/*");
+    editor.attachFile(uploadName, "image/jpeg", "environment");
   }
 }
 
 export async function attachDailyImage() {
   const uploadName = await editor.prompt("The name for the daily image:", `images/daily/${niceDate(new Date())}.jpg`);
   if (uploadName) {
-    editor.attachFile(uploadName!, "image/jpeg");
+    editor.attachFile(uploadName, "image/jpeg");
   }
+}
+
+export async function insertImage(environment: string | null = null) {
+  const fileName = await editor.attachFile(null, "image/*", environment);
+  console.log(`Attached file: ${fileName}`);
+  const cursorPos = await editor.getCursor();
+  const page = await editor.getCurrentPage();
+  await editor.insertAtCursor(`![](${fileName})`);
+}
+
+export async function insertCameraImage() {
+  await insertImage("environment");
 }
